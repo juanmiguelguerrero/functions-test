90cebddc404fe931900f67f6a3eec9109616d3f5
Juan Miguel Guerrero <juanmiguel@existo.es>
2020-04-06 11:22:02 +0200

    Versión con build

---
+++ src/functions/demo/demo.js
@@ -0,0 +1,25 @@
+const uniqueString = require('unique-string')
+const axios = require('axios')
+
+exports.handler = async (event, context) => {
+	
+	// event.path - The request path
+	// event.httpMethod - The request HTTP method
+	// event.headers - The request headers
+	// event.queryStringParameters - The request query parameters
+	// event.body - The request body in JSON format
+	// event.isBase64Encoded - A boolean flag to indicate if the applicable request payload is Base64-encode
+
+	console.log('Demo function called...')
+
+	return {
+
+		// isBase64Encoded: true|false,
+		// statusCode: httpStatusCode,
+		// headers: { "headerName": "headerValue", ... },
+		// body: "..."
+
+		statusCode: 200,
+		body: uniqueString()
+	}
+}
